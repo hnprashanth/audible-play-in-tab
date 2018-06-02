effe627dfb2ff70a6be2e2e68ce74e5e9753003c
Prashanth HN <prashanth@prashanth.net>
2018-06-02 20:33:51 +0530

    fix bugs

--- addlink.js
+++ addlink.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
-	var asins = []
+	var asins = [];
 	$("input[name='asin']").each(function() {
-		const asin = $(this).val()
+		const asin = $(this).val();
 		if (asins.indexOf(asin) == -1) {
-			asins.push(asin)
+			asins.push(asin);
 		}
 	})
 
 	$('.adbl-library-item').each(function(index) {
-		const thisAsin = asins[index]
+		const thisAsin = asins[index];
+		console.log(asins[index])
 		const bookUrl = "https://www.audible.com/cloudplayer?asin="+thisAsin;
-		$(this).after("<a href='"+bookUrl+"' target='_blank'><button>Play in Tab</button></a>")
+		$(this).after("<a href='"+bookUrl+"' target='_blank'><button>Play in Tab</button></a>");
 	})
-}
+
+})
