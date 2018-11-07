27a5dca9ed925b20d49f045d94e6f73ec3adaf06
Prashanth HN <prashanth@prashanth.net>
2018-11-07 12:55:03 +0530

    make it work for audible.in

--- addlink.js
+++ addlink.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
-	var asins = [];
-	$("input[name='asin']").each(function() {
-		const asin = $(this).val();
-		if (asins.indexOf(asin) == -1) {
-			asins.push(asin);
-		}
-	})
+  var asins = [];
+  $("input[name='asin']").each(function() {
+    const asin = $(this).val();
+    if (asins.indexOf(asin) == -1) {
+      asins.push(asin);
+    }
+  });
 
-	$('.adbl-library-item').each(function(index) {
-		const thisAsin = asins[index];
-		console.log(asins[index])
-		const bookUrl = "https://www.audible.com/cloudplayer?asin="+thisAsin;
-		$(this).after("<a href='"+bookUrl+"' target='_blank'><button>Play in Tab</button></a>");
-	})
-
-})
+  $(".adbl-library-item").each(function(index) {
+    const thisAsin = asins[index];
+    console.log(asins[index]);
+    const bookUrl =
+      "https://" + document.domain + "/cloudplayer?asin=" + thisAsin;
+    $(this).after(
+      "<a href='" +
+        bookUrl +
+        "' target='_blank'><button>Play in Tab</button></a>"
+    );
+  });
+});
