743bfa49ca6c84be5cef32ccab1b82c557997114
Prashanth HN <prashanth@prashanth.net>
2020-04-25 17:17:01 +0530

    fix change from cloudplayer t webplayer

--- addlink.js
+++ addlink.js
@@ -1,21 +1,20 @@
-$(document).ready(function() {
-  var asins = [];
-  $("input[name='asin']").each(function() {
-    const asin = $(this).val();
+$(document).ready(function () {
+  var asins = []
+  $("input[name='asin']").each(function () {
+    const asin = $(this).val()
     if (asins.indexOf(asin) == -1) {
-      asins.push(asin);
+      asins.push(asin)
     }
-  });
+  })
 
-  $(".adbl-library-item").each(function(index) {
-    const thisAsin = asins[index];
-    console.log(asins[index]);
-    const bookUrl =
-      "https://" + document.domain + "/cloudplayer?asin=" + thisAsin;
+  $('.adbl-library-item').each(function (index) {
+    const thisAsin = asins[index]
+    console.log(asins[index])
+    const bookUrl = 'https://' + document.domain + '/webplayer?asin=' + thisAsin
     $(this).after(
       "<a href='" +
         bookUrl +
-        "' target='_blank'><button>Play in Tab</button></a>"
-    );
-  });
-});
+        "' target='_blank'><button>Play in Tab</button></a>",
+    )
+  })
+})
