041746f4aa63d0ac5a367954aa6580f510a77c95
Prashanth HN <prashanth@antstack.com>
2024-01-08 13:37:53 +0530

    Add better styling to the button

--- addlink.js
+++ addlink.js
@@ -1,5 +1,22 @@
 $(document).ready(function () {
   var asins = []
+  var buttonStyle = `
+  <style>
+    .custom-play-button {
+      background-color: #FF9900;
+      padding: 10px 20px; /* Adjust space around the button */
+      border: none; /* Remove default border */
+      color: black; /* Text color */
+      cursor: pointer; /* Change cursor to indicate it's clickable */
+      margin: 5px; /* Space around the button */
+      transition: background-color 0.3s; /* Smooth transition for color change */
+    }
+    .custom-play-button:hover {
+      background-color: #FFB84D; /* Lighter shade on hover */
+    }
+  </style>
+`;
+  $('head').append(buttonStyle);  
   $("input[name='asin']").each(function () {
     const asin = $(this).val()
     if (asins.indexOf(asin) == -1) {
@@ -14,8 +31,8 @@ $(document).ready(function () {
     $(this).after(
       "<a href='" +
         bookUrl +
-        "' target='_blank'><button>Play in Tab</button></a>",
-    )
+        "' target='_blank'><button class='custom-play-button'>Play in Tab</button></a>",
+    );
   })
 
   // Play in tab on the page of the book
@@ -25,7 +42,7 @@ $(document).ready(function () {
     $(this).parent().after(
       "<a href='" +
         bookUrl +
-        "' target='_blank'><button>Play in Tab</button></a>",
-    )
+        "' target='_blank'><button class='custom-play-button'>Play in Tab</button></a>",
+    );
   })
 })
